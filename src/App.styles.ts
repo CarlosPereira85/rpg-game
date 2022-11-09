fd5a064599bf735f924fa5d66cdbb81a41452eba
CarlosPereira85 <caiqueberlin@gmail.com>
2022-11-09 13:30:54 +0100

    final

---
+++ src/App.styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: #24282F;
+    min-height: 100vh;
+    color: #FFF;
+`;
+
+export const Map = styled.div`
+    width: 480px;
+    height: 480px;
+  
+    background-image: url('/assets/map.png');
+    background-position: left top;
+    background-size: 100%;
+`;
+export const Map2 = styled.div`
+    width: 480px;
+    height: 480px;
+  
+    background-image: url('/assets/map2.png');
+   
+    background-size: 100%;
+`;
---
+++ src/types/CharacterSides.ts
@@ -0,0 +1,1 @@
+export type CharacterSides = 'down' | 'up' | 'left' | 'right';
